789191f7840c04e32eeb5649203d3df96078901a
jusmccar <justinmccartney1@yahoo.com>
2022-07-01 14:05:29 -0700

    Add ability to add and delete contacts Contacts preserve on refresh

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,40 @@
 import './App.css';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import uuid from "react-uuid";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
-  const [contacts, setContacts] = useState([]);
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)));
 
   const addContactHandler = (contact) => {
-    setContacts([...contacts, contact]);
+    setContacts([...contacts, { id: uuid(), ...contact }]);
   }
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList);
+  };
+
+  useEffect(() => {
+    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retrieveContacts) setContacts(retrieveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
     <div className="ui container">
       <Header />
       <AddContact addContactHandler={addContactHandler}/>
-      <ContactList contacts={contacts}/>
+      <ContactList contacts={contacts} getContactId={removeContactHandler}/>
     </div>
   );
 }
