1b3a2629cc697ecbcd84cf7184c5242db6b20cbb
jusmccar <justinmccartney1@yahoo.com>
2022-06-30 21:23:40 -0700

    Add useState React Hook

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,19 +1,44 @@
 import React from "react";
 
 class AddContact extends React.Component {
+    state = {
+        name: "",
+        email: ""
+    };
+
+    add = (event) => {
+        event.preventDefault();
+        if (this.state.name === "" && this.state.email === "") {
+            alert("All of the fields are mandatory.");
+            return;
+        }
+    }
+
     render() {
         return (
             <div className="ui main">
                 <h2>Add Contact</h2>
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.add}>
                     <div className="field">
                         <label>Name</label>
-                        <input type="text" name="name" placeholder="Name"></input>
+                        <input
+                            type="text"
+                            name="name"
+                            placeholder="Name"
+                            value={this.state.name}
+                            onChange={ (event) => this.setState({ name: event.target.value })}>
+                        </input>
                     </div>
 
                     <div className="field">
                         <label>Name</label>
-                        <input type="text" name="email" placeholder="Email"></input>
+                        <input
+                            type="text"
+                            name="email"
+                            placeholder="Email"
+                            value={this.state.email}
+                            onChange={ (event) => this.setState({ email: event.target.value })}>
+                        </input>
                     </div>
 
                     <button className="ui button blue">Add</button>
--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,11 @@
 import './App.css';
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
-  const contacts = [
-    {
-      id: "1",
-      name: "Justin",
-      email: "justin@gmail.com"
-    },
-    {
-      id: "2",
-      name: "John",
-      email: "johnny@gmail.com"
-    }
-  ];
+  const [contacts, setContacts] = useState([]);
 
   return (
     <div className="ui container">
