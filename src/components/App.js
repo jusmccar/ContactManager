f4d2b5a96f3216a1f8f9e2f639ba8c81e3d6f924
jusmccar <justinmccartney1@yahoo.com>
2022-06-30 20:23:04 -0700

    Add initial components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import React from "react";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Header />
+      <AddContact />
+      <ContactList />
+    </div>
   );
 }
 
