c14145dca69649b74bcbc68726febf7c904ba39f
jusmccar <justinmccartney1@yahoo.com>
2022-07-07 19:31:17 -0700

    Add null check for localStorage.getItem

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import ContactList from "./ContactList";
 
 function App() {
   const LOCAL_STORAGE_KEY = "contacts";
-  const [contacts, setContacts] = useState(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)));
+  const [contacts, setContacts] = useState(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? []);
 
   const addContactHandler = (contact) => {
     setContacts([...contacts, { id: uuid(), ...contact }]);
